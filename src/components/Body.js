8d80245995d1b34d1bf09c267d4b7bf2d692f8f1
Gaurav <gouraw1@gmail.com>
2023-12-25 19:43:47 +0530

    create filter button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList)
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurant</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterdList = listOfRestaurant.filter(
+              (res) => res.info.avgRatingString > 4
+            );
+            setListOfRestaurant(filterdList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
